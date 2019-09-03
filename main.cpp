05ce484a08a8f434c20b9b8503c6c217d69b6ecf
bit-savoki <401471767@qq.com>
2019-09-03 21:22:46 +0800

    ‘更新到原作者的主分支’

--- main.cpp
+++ main.cpp
@@ -1,14 +1,15 @@
 #include "mainwindow.h"
+#include "ui_mainwindow.h"
 #include <QApplication>
 #include<QTextCodec>
+#include<QTreeWidget>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     QTextCodec::setCodecForLocale(QTextCodec::codecForLocale());
     MainWindow w;
+
     w.show();
-
-
     return a.exec();
 }
